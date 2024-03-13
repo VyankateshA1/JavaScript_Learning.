6c39d968b8227f1f6e3339ab7918b192f8c7d606
VyankateshA1 <vyankateshandhale@gmail.com>
2024-03-13 09:46:05 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+//Variables
+const accountId = 1211;
+let accountEmail = "123abc@gmai.com"
+var accountPassword = "123321"
+
+accountEmail = "abc@gmail.com"
+accountPassword = "1111"
+let userName;
+
+//dont use var because of block scope
+console.log(accountId)
+console.table([userName,accountId,accountEmail,accountPassword])
