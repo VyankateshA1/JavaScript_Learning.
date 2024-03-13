f27faf8c99e113e8afdcf0632de898c87361059f
VyankateshA1 <vyankateshandhale@gmail.com>
2024-03-13 12:30:28 +0530

    converstion

--- 01_basics/03_converstionOperation.js
+++ 01_basics/03_converstionOperation.js
@@ -14,8 +14,31 @@ let valueNum = Number(score)
 // let booleanLog = Boolean(isLogged)
 // console.log(booleanLog)
 
-let num = 22
-let stringNum = String(num);
-console.log(stringNum);
+// let num = 22
+// let stringNum = String(num);
+// console.log(stringNum);
+
+
+// ****************************  Operations  **************//
+let value = 3
+let negValue = -value
+// console.log(negValue);
+console.log(2+2)
+
+let str1 = "hello"
+let str2 = "Go'od Morming.."
+
+let str3 = str1 + " "+ str2
+
+console.log(str3);
+console.log("1"+2)
+console.log("1"+"2");
+console.log(1+2+"2");
+
+let game = 100
+++game;
+console.log(game)
+
+
 
 
