69e94295f54692bfb18dc7d8aaca033096f3779d
VyankateshA1 <vyankateshandhale@gmail.com>
2024-03-13 11:20:47 +0530

    convertion Operations

---
+++ 03_converstionOperation.js
@@ -0,0 +1,21 @@
+// let score = "201bb1"
+// let score = null
+//let score = undefined
+let score = "121m"
+
+//console.log(typeof score);
+
+let valueNum = Number(score)
+
+// console.log(typeof valueNum)
+// console.log(valueNum);
+
+// let isLogged = "" //1 = True // 0 = false // " "= false
+// let booleanLog = Boolean(isLogged)
+// console.log(booleanLog)
+
+let num = 22
+let stringNum = String(num)
+console.log(stringNum);
+
+
