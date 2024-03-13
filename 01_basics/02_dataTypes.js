75af8ffc819b54b0a3eab8ae0a78626ffec73f34
VyankateshA1 <vyankateshandhale@gmail.com>
2024-03-13 10:01:40 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer Version
+
+//alert("Hello")
+
+console.log(3+3)
+
+let myName = "Vyankatesh"
+let age = 24
+let student = true
+
+//number
+//bigInt
+// string = ""
+// Boolean = true/false
+// null = standalone CSSMathValue
+// undefined = 
+// Symbol = unique
+
+
+//Object
+
+console.log(typeof(myName))
+console.log(typeof null) // Object
+console.log(typeof undefined)
