09934f1df94dad7fa232bf7dcd8287fb6d52543a
VyankateshA1 <vyankateshandhale@gmail.com>
2024-03-15 13:16:29 +0530

    Control Flow if_else

--- 04_Control Flow/01_control.js
+++ 04_Control Flow/01_control.js
@@ -18,3 +18,33 @@ if(temp > 25){
 }else{
   console.log('today is colder');
 }
+
+/**************** short hand notetion *****************************/
+
+const balance = 1000
+//if(balance > 500) console.log('add more');
+if (balance < 500) {
+  console.log('less than 500');
+}else if(balance < 800){
+  console.log('less than 800');
+}else{
+  console.log('less than 1100');
+}
+
+/*********************** &&  *******************************************/
+
+const name = true
+const age = 18
+
+if (name && age === 18) {
+  console.log('You can give driving licene test');
+}
+
+/**************************** || **********************************/
+
+const googleislogged = true
+const yahooislogged = false
+
+if (googleislogged || yahooislogged) {
+  console.log('You have been logged in');
+}
