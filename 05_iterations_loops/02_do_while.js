eea1c3442e799ea876b2815eedd1257ff0895060
VyankateshA1 <vyankateshandhale@gmail.com>
2024-03-15 23:21:37 +0530

    do_while

---
+++ 05_iterations_loops/02_do_while.js
@@ -0,0 +1,20 @@
+
+let index = 0
+while (index <= 10) {
+  //console.log(`value of ${index}`);
+  index ++
+}
+
+let myarr = ['hello','byee','welcome']
+let arr = 0
+while (arr < myarr.length) {
+  //console.log(`value is ${myarr[arr]}`);
+  arr++
+}
+
+/************ do while ***************/
+let mark = 10
+do {
+  console.log(`marks is ${mark}`);
+  mark++
+} while (mark <= 20);
