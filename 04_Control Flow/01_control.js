3000d4d39fd2d7c1a0acbbfe208502676b7ac1b5
VyankateshA1 <vyankateshandhale@gmail.com>
2024-03-15 12:52:25 +0530

    if_else

---
+++ 04_Control Flow/01_control.js
@@ -0,0 +1,20 @@
+// if 
+/*if(condition){
+
+}*/ 
+
+const isPresent = true;
+
+if(isPresent){
+  console.log(true);
+}
+/**Camparision Operatir
+ * <,>,<=,>=,==,===,!=,
+ */
+
+let temp = 39 
+if(temp > 25){
+  console.log('today is hot outside🔥');
+}else{
+  console.log('today is colder');
+}
