4aa1e23df9804399607e924af50ac5345f5a93ad
VyankateshA1 <vyankateshandhale@gmail.com>
2024-03-15 13:27:07 +0530

    switch

---
+++ 04_Control Flow/02_switch.js
@@ -0,0 +1,17 @@
+
+let fruit = 'DragonFruit2';
+switch (fruit ) {
+  case 'Mango':
+    console.log('summer fruit');
+    break;
+  case 'Banana':
+    console.log('High in carb');
+    break;
+  case 'DragonFruit':
+    console.log('help to increse white cell');
+    break;
+ 
+  default:
+    console.log('Eat Fruits everyDay');
+    break;
+}
