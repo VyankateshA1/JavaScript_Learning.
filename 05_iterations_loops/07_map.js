31cfee21bf76636f8dd82873edcddd2a7b8be60f
VyankateshA1 <vyankateshandhale@gmail.com>
2024-03-16 09:52:16 +0530

    map

---
+++ 05_iterations_loops/07_map.js
@@ -0,0 +1,14 @@
+const num = [1,2,3,4,5,6,7,8,9,10]
+
+const add = num.map((num)=> num+10)
+//console.log(add);
+const multiply = num.map((num)=> {return num*10})
+//console.log(multiply);
+
+
+// 
+const newNum = num
+                .map((num)=> num *10)
+                .map((num)=> num+2)
+                .filter((num)=>num >= 40)
+              console.log(newNum);
