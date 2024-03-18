f8526858faeb55bbb22f4b070bef9edd782c8ff7
VyankateshA1 <vyankateshandhale@gmail.com>
2024-03-18 11:04:14 +0530

    Dom project

--- 08_Dom Project -2/script.js
+++ 08_Dom Project -2/script.js
@@ -6,7 +6,8 @@ form.addEventListener("submit", function (e) {
   const height = parseInt(document.querySelector("#height").value);
   const weight = parseInt(document.querySelector("#weight").value);
   const result = document.querySelector("#result");
-
+  const msg = document.querySelector(".msg");
+  
   if (height === "" || height < 0 || isNaN(height)) {
     result.innerHTML = "Please give a valid height";
   } else if (weight === "" || weight < 0 || isNaN(weight)) {
@@ -15,8 +16,19 @@ form.addEventListener("submit", function (e) {
     const bmi = Math.round(weight / ((height * height) / 10000).toFixed(2));
     //result show
     result.innerHTML = `<span> BMI = ${bmi}</span>`;
-    if (result < "20") {
-      result.innerHTML = "very low";
+
+    if (bmi < 16) {
+      msg.innerHTML = "(Severe Thinness)";
+    } else if (bmi <= 17) {
+      msg.innerHTML = "(Moderate Thinness)";
+    } else if (bmi <= 18.5) {
+      msg.innerHTML = "(Mild Thinness)";
+    } else if ((bmi) => 18.5 || bmi < 25) {
+      msg.innerHTML = "(Normal)";
+    } else if (bmi > 25 || bmi < 30) {
+      msg.innerHTML = "(Overweight)";
+    } else {
+      msg.innerHTML = "(Obese)";
     }
   }
 });
